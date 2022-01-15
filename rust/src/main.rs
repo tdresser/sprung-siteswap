0c9c334022320014a3a7c527b4d3f4c74435f77d
Tim Dresser <tdresser@gmail.com>
2022-01-15 11:39:36 -0500

    Bases working.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,22 +2,64 @@ use pest::{iterators::Pairs, Parser};
 use pest_derive::Parser;
 
 #[derive(Parser)]
-#[grammar = "grammar.pest"]
+#[grammar = "normalize.pest"]
 struct SSParser;
 
-fn print_pairs(pairs: Pairs<Rule>) {
+#[derive(Debug)]
+struct Pattern {
+    nonzip_positions: Vec<String>,
+    siteswap: Vec<u32>,
+}
+
+fn normalize(pairs: Pairs<Rule>, pattern: &mut Pattern) {
     for pair in pairs {
         match pair.as_rule() {
-            Rule::notation => print_pairs(pair.into_inner()),
-            Rule::ss => print_pairs(pair.into_inner()),
-            Rule::digit => print!("<{}>", pair.as_str()),
+            Rule::notation => normalize(pair.into_inner(), pattern),
             Rule::s => print!("{}", pair.as_str()),
+            Rule::base => {
+                pattern.siteswap = match pair.as_str().chars().nth(0).unwrap() {
+                    'B' => vec![2u32],
+                    'C' => vec![3u32],
+                    'F' => vec![4u32],
+                    'S' => {
+                        let mut chars = pair.as_str().chars();
+                        // Skip over the S.
+                        chars.next();
+                        println!("CHARS: {:?}", chars);
+                        let digits = chars.map(|x: char| x.to_digit(16).unwrap());
+                        digits.collect::<Vec<_>>()
+                    }
+                    _ => unreachable!(),
+                };
+            }
+            Rule::position => println!("position: {}", pair.as_str()),
+            Rule::digit => unreachable!(),
+            Rule::ss => unreachable!(),
         };
     }
 }
 
-fn main() {
-    let pairs = SSParser::parse(Rule::notation, "S312").unwrap_or_else(|e| panic!("{}", e));
+fn parse(s: &str) -> Pattern {
+    let mut pattern = Pattern {
+        nonzip_positions: vec![],
+        siteswap: vec![],
+    };
+    let pairs = SSParser::parse(Rule::notation, s).unwrap_or_else(|e| panic!("{}", e));
+    normalize(pairs, &mut pattern);
+    return pattern;
+}
 
-    print_pairs(pairs);
+fn main() {
+    let mut pattern = parse("cS312");
+    println!("{:?}\n", pattern);
+    pattern = parse("cB");
+    println!("{:?}\n", pattern);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
