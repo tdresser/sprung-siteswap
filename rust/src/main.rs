bfebd824da826547b4eae493030173333fcc5b43
Tim Dresser <tdresser@gmail.com>
2022-01-14 19:40:56 -0500

    Super simple grammar

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,23 @@
+use pest::{iterators::Pairs, Parser};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct SSParser;
+
+fn print_pairs(pairs: Pairs<Rule>) {
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::notation => print_pairs(pair.into_inner()),
+            Rule::ss => print_pairs(pair.into_inner()),
+            Rule::digit => print!("<{}>", pair.as_str()),
+            Rule::s => print!("{}", pair.as_str()),
+        };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pairs = SSParser::parse(Rule::notation, "S312").unwrap_or_else(|e| panic!("{}", e));
+
+    print_pairs(pairs);
 }
