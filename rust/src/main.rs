c62be791798f4cd70bdf65f05d07c28a5a4244b4
Tim Dresser <tdresser@gmail.com>
2022-01-15 19:32:28 -0500

    Split full and short

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -11,7 +11,7 @@ struct Pattern {
     siteswap: Vec<u32>,
 }
 
-fn normalize(pairs: Pairs<Rule>, pattern: &mut Pattern) {
+/*fn normalize(pairs: Pairs<Rule>, pattern: &mut Pattern) {
     for pair in pairs {
         match pair.as_rule() {
             Rule::notation => normalize(pair.into_inner(), pattern),
@@ -35,20 +35,63 @@ fn normalize(pairs: Pairs<Rule>, pattern: &mut Pattern) {
             Rule::s | Rule::ss | Rule::digit => unreachable!(),
         };
     }
-}
+}*/
 
 fn parse(s: &str) -> Pattern {
+    println!("{}", s);
     let mut pattern = Pattern {
         nonzip_positions: vec![],
         siteswap: vec![],
     };
-    let pairs = SSParser::parse(Rule::notation, s).unwrap_or_else(|e| panic!("{}", e));
-    normalize(pairs, &mut pattern);
+    let mut pairs = SSParser::parse(Rule::notation, s).unwrap_or_else(|e| panic!("{}", e));
+    let top = pairs.next().unwrap();
+    assert!(pairs.next() == None);
+
+    //let top = &pairs[0];
+    //let top = pairs[0].as_rule();
+
+    if top.as_rule() == Rule::shortnotation {
+        println!("Short");
+        for pair in top.into_inner() {
+            println!("{}", pair);
+        }
+    } else if top.as_rule() == Rule::fullnotation {
+        println!("Full");
+
+        for pair in top.into_inner() {
+            println!("{}", pair);
+        }
+    }
+
+    /*for pair in pairs {
+        match pair.as_rule() {
+            Rule::notation => normalize(pair.into_inner(), pattern),
+            Rule::base => {
+                pattern.siteswap = match pair.as_str().chars().nth(0).unwrap() {
+                    'B' => vec![2u32],
+                    'C' => vec![3u32],
+                    'F' => vec![4u32],
+                    'S' => {
+                        let mut chars = pair.as_str().chars();
+                        // Skip over the S.
+                        chars.next();
+                        println!("CHARS: {:?}", chars);
+                        let digits = chars.map(|x: char| x.to_digit(16).unwrap());
+                        digits.collect::<Vec<_>>()
+                    }
+                    _ => unreachable!(),
+                };
+            }
+            Rule::position => pattern.nonzip_positions = vec![pair.as_str().to_string()],
+            Rule::s | Rule::ss | Rule::digit => unreachable!(),
+        };
+    }*/
+
     return pattern;
 }
 
 fn main() {
-    let mut pattern = parse("cS312");
+    let mut pattern = parse("czS312");
     println!("{:?}\n", pattern);
     pattern = parse("cB");
     println!("{:?}\n", pattern);
---
+++ rust/src/thoughts.md
@@ -0,0 +1,2 @@
+cS2 us ambiguous IMO, so I'd have Sc2 or czS2
+
